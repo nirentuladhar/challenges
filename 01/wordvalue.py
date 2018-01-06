ef8cbdad73ab14b51cebcf639933b7c18a3962a2
Niren <nirentuladhar@gmail.com>
2018-01-06 19:57:16 +1100

    Exercise 01

---
+++ 01/wordvalue.py
@@ -0,0 +1,25 @@
+from data import DICTIONARY, LETTER_SCORES
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    return open("dictionary.txt").read().splitlines()
+     
+def calc_word_value(word):
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    score = 0
+    for letter in word:
+        try: score += int(LETTER_SCORES[letter.upper()])
+        except: print("Unknown character '{}' in the word: {}.".format(letter, word))
+    return score
+
+def max_word_value(words = load_words()):
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    word_score = {}
+    for word in words: word_score[word] = calc_word_value(word)
+    return max(word_score, key=word_score.get)
+
+
+if __name__ == "__main__":
+    pass # run unittests to validate
